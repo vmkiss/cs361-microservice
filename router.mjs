afe56f60a01f7822e87af24688f50705a86f7f87
vmkiss <veronika.kiss17@gmail.com>
2024-11-10 18:02:53 -0800

    create post route

--- router.mjs
+++ router.mjs
@@ -24,6 +24,26 @@ router.get("/:userID/dates", async (req, res) => {
     }
 })
 
+// Create new event
+router.post("/", async(req, res) => {
+    try {
+        const { userId, attendees, startEvent, endEvent, title } = req.body;
+
+        const event = await Event.create({
+            userID,
+            attendees,
+            startEvent,
+            endEvent,
+            title
+        })
+        res.status(200).json(event)
+    } catch (error) {
+        res.status(400).json({
+            error: "Invalid request from client for event creation operation.",
+        })
+    }
+});
+
 
 
 export { router }
