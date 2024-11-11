5edacddd54b2d07851c501ffab877a18b52c8963
vmkiss <veronika.kiss17@gmail.com>
2024-11-10 18:08:44 -0800

    create delete route

--- router.mjs
+++ router.mjs
@@ -44,6 +44,23 @@ router.post("/", async(req, res) => {
     }
 });
 
+// Delete an event
+router.delete("/:id", (req, res) => {
+    const { id } = req.params
+    try {
+        const event = await Event.findOneAndDelete({ _id: id})
+        if (!event) {
+            return res.status(404).json({
+                error: "Unable to locate event with that id for delete operation.",
+            })
+        }
+        res. status(200).json(patient)
+    } catch (error) {
+        res.status(400.).json({
+            error: "Invalid request from client for event delete operation.",
+        })
+    }
+})
 
 
 export { router }
