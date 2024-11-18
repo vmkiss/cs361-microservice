c24beaa8cdbd6c0e51f96e5dda048454dbb2fcee
vmkiss <veronika.kiss17@gmail.com>
2024-11-18 15:16:06 -0800

    test create, delete, and get functionalities -all passed

--- test-program/index.mjs
+++ test-program/index.mjs
@@ -7,9 +7,9 @@ const createEvent = async () => {
         const response = await axios.post(`${BASE_URL}/events`, {
             "userID": "1",
             "attendees": ["3",],
-            "startEvent": "2024-11-14T15:30:00",
-            "endEvent": "2024-11-14T16:30:00",
-            "title": "John Doe Appointment",
+            "startEvent": "2025-11-14T15:30:00",
+            "endEvent": "2025-11-14T16:30:00",
+            "title": "Tom Sawyer Appointment",
         });
 
         console.log('Event created successfully:', response.data);
@@ -25,8 +25,8 @@ const createEvent = async () => {
 const getEvent = async() => {
     try {
         const userID = 1;
-        const startEvent = "2024-11-14T15:30:00";
-        const endEvent = "2024-11-14T16:30:00";
+        const startEvent = "2025-11-14T15:30:00";
+        const endEvent = "2025-11-14T16:30:00";
 
         const response = await axios.get(`${BASE_URL}/events/${userID}/dates`, {
             params: { start: startEvent, end: endEvent },
@@ -44,7 +44,7 @@ const getEvent = async() => {
 
 const deleteEvent = async() => {
     try {
-        const deleteID = "673bc139c744404a1b60e18a";
+        const deleteID = "673bc90b73cd802e1dd3ce8c";
 
         const response = await axios.delete(`${BASE_URL}/events/${deleteID}`);
 
