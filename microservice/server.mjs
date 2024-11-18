263bf1f0b488e17eb6fb937f9c3c84c75d5e5bd2
vmkiss <veronika.kiss17@gmail.com>
2024-11-18 13:56:59 -0800

    add cors dependency to both package.json files

--- microservice/server.mjs
+++ microservice/server.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import './events-model.mjs';
 import { router } from './router.mjs';
 
 const PORT = process.env.PORT;
 const app = express();
+//app.use(cors({credentials: true, origin: "*"}))
 app.use(express.json());
 app.use('/events', router)
 
